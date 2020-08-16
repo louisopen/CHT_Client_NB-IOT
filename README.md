9c69a40c18c6c72ec052920de8a91c17999a9801
Louis Huang <louisopen@gmail.com>
2020-08-16 18:57:53 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# NB-IOT_Client 
-SIM7000C NB-IOT module for client applications test base on Raspberry Pi
+### NB-IOT_Client 
+
+> SIM7000C of NB-IOT module client on Raspberry Pi applications, SIM7000C 運行在樹莓派上的應用
+> SIM card(大卡) 採用中華電信CAT. M1 門市申請即可
+
+#### 1. Close power-saving of the display on Raspberry Pi
+#### 先關閉視窗下螢幕休眠功能(當然也可以不用關閉)
+
+sudo nano /etc/lightdm/lightdm.conf 
+* [SeatDefaults]
+* ...
+* #xserver-command=X	    #Change to as below
+* xserver-command=X -s 0 –dpms
+* ...
+
+#### 2. How to login command line mode to run application in the X-windows mode? 
+#### 視窗模式開機自動開啟一個終端機模式, 並運行應用程序
+
+sudo nano /home/pi/.config/autostart/autoboot.desktop 
+* ...
+* [Desktop Entry]
+* Type=Application
+* Name=MyApplicationRun
+* #Exec = lxterminal -e /home/pi/autorun.sh               #運行掛機 applivcation close aftern run 5day 
+* Exec = lxterminal -e "bash -c /home/pi/autorun.sh;bash" #運行測試(Stay Bash)
+* Icon=pictur.png
+* ...
+or
+
+* [Desktop Entry]
+* Encoding=UTF-8
+* Type=Application
+* Name=myprogram
+* Exec=lxterminal -e bash -c '/home/pi/autorun.sh;$SHELL' #bash run autorun.sh
+* Terminal=true
+* ...
+
+* sudo nano /home/pi/autorun.sh
+* ...
+* python3 SIM7000iot.cht.py
+* ...
+
+#### 3. SIM7000Ciot.cht.py
+
+
--- SIM7000iot.cht.py
+++ SIM7000iot.cht.py
@@ -14,32 +14,49 @@
 
 import  RPi.GPIO as GPIO
 import time
-import serial   
+import serial
 #import threading
+import code 
+
+ser=''
 
 def gpio_init():
     GPIO.setwarnings(False)
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(23,GPIO.OUT)
     
-#echo 'AT+SAPBR=2,1\r\n' > /dev/ttyAMA0
-#cat /dev/ttyAMA0
-
-def receiving(ser):
-    ser.flushInput()    #清除接收緩衝區
+#echo 'AT+SAPBR=2,1\r\n' > /dev/ttyAMA0     #Under bash
+def receiving(ser):             #寫法簡單, 但有坑! 先頂著!
+    #ser.flushInput()           #清除接收緩衝區
     last_received=''
-    while True:
-        time.sleep(0.2) #時間太短, 字元來不及傳回, 斷字
+    time.sleep(0.1)       
+    count = ser.inWaiting()     #取得當下緩衝區字元數
+    while count != 0:
+        last_received += ser.read(count) 
+        time.sleep(0.125)       #
         count = ser.inWaiting() #取得當下緩衝區字元數
-        if count != 0:
-            last_received = ser.read(count) 
-            #print(last_received)
-            break
+    last_received += ser.read(count) 
     return last_received
 
+def lte_test():
+    ser.write('AT\r\n')         #同步測試
+    print(receiving(ser))
+    time.sleep(0.5)  
+    ser.write('AT+CPIN?\r\n')   #SIM-car ?
+    print(receiving(ser))
+    time.sleep(0.5)
+    ser.write('AT+CSQ\r\n')     #查询信号强度 #查詢訊號品質Rssi,Ber
+    print(receiving(ser))
+    time.sleep(0.5)
+    ###########透過 AT+CGATT? & AT+CPSI? 指令來確認已連線基地台###########
+    ser.write('AT+CPSI?\r\n')   #查詢 註冊訊息
+    print(receiving(ser))
+    time.sleep(0.5)
+    ser.write('AT+CGATT?\r\n')  #查询业务是否附着，确保卡不欠费
+    print(receiving(ser))
 
-
-if __name__ == '__main__':
+def init_comm():
+    global ser
     gpio_init()
     ser = serial.Serial("/dev/ttyAMA0",115200)  #Pi2
     #ser = serial.Serial("/dev/ttyS0",115200)  #Pi3 
@@ -48,70 +65,89 @@ if __name__ == '__main__':
         exit(0)
     #st1 = threading.Thread(target=receiving, args=(ser,))
     #st1.start()
-    time.sleep(0.2)
+    ser.write('AT+CIFSR\r\n')               #查詢本地IP (Get local IP)
+    print(receiving(ser))
+
+def lte_setup():
+    ser.write('AT+CNMP=38\r\n') #LTE only
+    print(receiving(ser))
+    time.sleep(0.5)
+    ser.write('AT+CMNB=1\r\n')  #LTE(1:Cat-M1 and 3:NB-IOT)
+    print(receiving(ser))
+    time.sleep(0.5)
+    ser.write('AT+NBSC=1\r\n')  #開啟擾碼
+    print(receiving(ser))
+    time.sleep(0.5)
+    ################連線IP網路#################
+    ser.write('at+cstt="internet.iot"\r\n') #連到internet網路
+    print(receiving(ser))
+    time.sleep(4)
+    ser.write('at+CIICR\r\n')               #啟動IP數據網路
+    print(receiving(ser))
+
+def localip():
+    ser.write('AT+CIFSR\r\n')               #查詢本地IP (Get local IP)
+    print(receiving(ser))
+
+def ping50():
+    ser.write('AT+CIPPING="168.95.1.1",50\r\n') 
+    #ser.write('AT+CIPPING="8.8.8.8"\r\n')
+    print(receiving(ser))
+
+def function():
     try:    
-        ser.write('AT\r\n')        #同步測試
-        print(receiving(ser))
-        time.sleep(0.5)  
-        ser.write('AT+CPIN?\r\n')   #SIM-car ?
-        print(receiving(ser))
+        lte_test()
         time.sleep(0.5)
-        ser.write('AT+CNMP=38\r\n') #LTE only
-        print(receiving(ser))
+        lte_setup()
         time.sleep(0.5)
-        ser.write('AT+CMNB=3\r\n') #LTE(Cat-M1 and NB-IOT)
-        print(receiving(ser))
-        time.sleep(0.5)
-        #ser.write('AT+NBSC=1\r\n') #開啟擾碼
-        #print(receiving(ser))
-        #time.sleep(0.5)
-        ser.write('AT+CPSI?\r\n')   #查詢 註冊訊息
-        print(receiving(ser))
-        time.sleep(1)
-        ser.write('AT+CGATT?\r\n')  #查詢 是否連卓
-        print(receiving(ser))
-        time.sleep(1)
-        #ser.write('AT+COPS?\r\n') #查詢(index,format,oper) Scan cells
-        #print(receiving(ser))
-        #time.sleep(1)
-        ser.write('AT+CSQ\r\n')     #查詢Rssi,Ber
-        print(receiving(ser))
-        time.sleep(1)
+        '''
         ser.write('AT+CGNAPN\r\n')  #查詢APN
         print(receiving(ser))
         time.sleep(1)
 
-        #ser.write('at+cops=1,2,”46692”\r\n')   #Connect to 中華電信
+        ser.write('AT+COPS?\r\n')  #查詢掃描基地台(index,format,oper),(9 is NB-IOT,7 is CAT-M1)
+        print(receiving(ser))
+        time.sleep(1)
+        '''
         #ser.write('AT+SAPBR=3,1,"APN","CMNET"\r\n')  #中国移動
         #ser.write('AT+SAPBR=3,1,"APN","internet"\r\n')  #中華電信 NB-IOT (低速)
         #ser.write('AT+SAPBR=3,1,"APN","internet.iot"\r\n')  #中華電信 Cat-M1  
-        #ser.write('AT+CGDCONT=1,"IP","internet.iot"\r\n')
-        ser.write('at+cstt="internet.iot"\r\n')
+
+        ser.write('AT+CGDCONT=1,"IP","internet.iot"\r\n')   #設定電信公司APN
+        #ser.write('AT+CGDCONT=1,"IP",""\r\n')              #自動(由基地台決定)
         print(receiving(ser))
         time.sleep(4)
 
-        ser.write('at+cops?\r\n')   #9 is NB-IOT,7 is CAT-M1
+        ser.write('at+cops=1,2,”46692”\r\n')   #連上基地台(中華電信), 第一次會花很長時間
         print(receiving(ser))
         time.sleep(1)
 
-        ############啟用移動場景############
-        ser.write('AT+CIMI\r\n') 
+        ################啟用移動場景###############
+        #ser.write('AT+CIMI\r\n') 
+        #print(receiving(ser))
+        #time.sleep(1)
+        ###############查看一下IP##################
+        #ser.write('AT+SAPBR=2,1\r\n')          #To query the IP
+        ser.write('AT+CIFSR\r\n')               #查詢IP (Get local IP)
         print(receiving(ser))
         time.sleep(1)
-        #############查看一下IP#############
-        #ser.write('AT+SAPBR=2,1\r\n')  #To query the IP
-        ser.write('AT+CIFSR\r\n') #Get local IP
-        print(receiving(ser))
-        time.sleep(1)
-        ##############用ping測試############
+
+
+        ##############應用1. ping測試##############
         ser.write('AT+CIPPING="168.95.1.1"\r\n') 
+        #ser.write('AT+CIPPING="8.8.8.8"\r\n')
         print(receiving(ser))
         time.sleep(1)
-        #############用TCP連線測試###########
+        #############應用2. TCP連線測試############
         ser.write('at+cipstart=”tcp”,”ftp.isu.edu.tw”,21\r\n') 
+        #ser.write('AT+CIPSTART="TCP","123.123.123.123",9487\r\n')
         print(receiving(ser))
         time.sleep(1)
-        
+        #############應用3. 連線測試############
+        ser.write('AT+CIPSEND=I love you\r\n') 
+        print(receiving(ser))
+        time.sleep(1)
+
         status=1
         while True:
             if GPIO.input(23) == True:
@@ -140,3 +176,8 @@ if __name__ == '__main__':
     finally:
         #st1.join()
         GPIO.cleanup() 
+
+if __name__ == '__main__':
+    #init_comm()
+    #function()
+    code.interact(local=locals())
